fdd215d88a9cad645ea58172eedc53a10210c1ba
John Faria <johnwbf@hotmail.com>
2025-04-27 16:51:31 -0300

    fix: adjust ConfigProtocol attributes

--- src/core/domain/protocols/config_protocol.py
+++ src/core/domain/protocols/config_protocol.py
@@ -4,5 +4,6 @@ from typing import Protocol, runtime_checkable
 @runtime_checkable
 class ConfigProtocol(Protocol):
     database_url: str
+    database_name: str
     secret_key: str
-    envinroment: str
+    environment: str
